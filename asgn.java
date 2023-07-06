3c239ebb02888753f566ebc397a65a2841919e93
Murathanisk <murathan.isik@ug.bilkent.edu.tr>
2023-07-06 16:27:39 +0300

    asdasd
    asdasd

---
+++ asgn.java
@@ -0,0 +1,5 @@
+public class asgn {
+    public static void main(String[] args) {
+        System.out.println("1. Prime Numbers \n2. Volume Filling \n3. Union Area \n4. Random Questions \n5. Exit");
+    }
+}
